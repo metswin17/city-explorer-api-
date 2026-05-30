9aab02dadf89a328f45215aa65675ce87270d892
Luis Lopez <metfanforever@gmail.com>
2026-05-30 07:57:19 -0700

    add files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express'); 
+const cors = require('cors'); 
+require('dotenv').config(); 
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
