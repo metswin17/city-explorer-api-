3160338c223e5b056b2228174283eb4ca81a617e
Luis Lopez <metfanforever@gmail.com>
2026-05-31 15:20:28 -0700

    mod server

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 require('dotenv').config(); 
 const app = express();
 app.use(cors());
+const weatherData = require('./data/weather.json');
 const PORT = process.env.PORT || 3001;
+
+console.log(weatherData);
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
