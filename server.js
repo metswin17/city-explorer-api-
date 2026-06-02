76277334022c31bc0ecd55f32f31b851112ad45d
Luis Lopez <metfanforever@gmail.com>
2026-06-02 00:04:46 -0700

    mod server

--- server.js
+++ server.js
@@ -4,12 +4,29 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 const weatherData = require('./data/weather.json');
-const PORT = process.env.PORT || 3001;
 
 console.log(weatherData);
+const PORT = process.env.PORT || 3001;
+
+class Forecast {
+  constructor(dayObj) {
+    this.date = dayObj.valid_date;
+
+    this.description =
+      `Low of ${dayObj.low_temp},
+      high of ${dayObj.max_temp}
+      with ${dayObj.weather.description}`;
+  }
+}
+
+const { lat, lon, searchQuery } = request.query;
 
 app.get('/', (req, res) => {
   res.send('Server is running');
+});
+
+app.get('/weather', (request, response) => {
+
 });
 
 app.listen(PORT, () => {
